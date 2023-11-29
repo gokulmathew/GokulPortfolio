3ed7999a37a910d9be96bcd24891e5e43d4fca84
sgokul <sgokul@gartner.com>
2023-11-29 20:22:56 +0530

    blogs updated

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,35 +1,57 @@
 export default function Blog() {
   return (
-    <div className="contaner my-5">
-      <div className="row">
-        <div className="col-12">
-          <h1>Blog</h1>
+    <div className='contaner my-5'>
+      <div className='row'>
+        <div className='col-12'>
+          <h1>Blogs</h1>
 
           <a
-            className="my-5  anchor d-block"
-            href="https://medium.com/@gokul.srinivasan22/lets-explore-about-react-js-fb5ce4ebb78c"
-            target="_blank"
+            className='my-5 anchor d-block'
+            href='https://medium.com/@gokul.srinivasan22/efficient-and-cost-effective-hosting-your-frontend-app-on-aws-with-s3-and-cloudfront-4563639b6b36'
+            target='_blank'
           >
-            <h3>What is React JS</h3>
-            <p className="mb-0 text-xs">Apr 20, 2020</p>
+            <h3>
+              Efficient and Cost-Effective: Hosting Your Frontend App on AWS
+              with S3 and CloudFront
+            </h3>
+            <p className='mb-0 text-xs'>Nov 29, 2023</p>
           </a>
 
           <a
-            className="my-5  anchor d-block"
-            href="https://medium.com/@gokul.srinivasan22/react-hooks-and-its-advantages-c54471da806"
-            target="_blank"
+            className='my-5 anchor d-block'
+            href='https://medium.com/@gokul.srinivasan22/demystifying-microservices-unleashing-the-power-of-modular-architectures-2edf13b25bbf'
+            target='_blank'
           >
-            <h3>React Hooks and its Advantages</h3>
-            <p className="mb-0 text-xs">Jun 20, 2020</p>
+            <h3>
+              Demystifying Microservices: Unleashing the Power of Modular
+              Architectures
+            </h3>
+            <p className='mb-0 text-xs'>Nov 29, 2023</p>
           </a>
 
           <a
-            className="my-5 anchor d-block"
-            href="https://medium.com/@gokul.srinivasan22/storybook-and-its-addons-bdadaf73b4c3"
-            target="_blank"
+            className='my-5 anchor d-block'
+            href='https://medium.com/@gokul.srinivasan22/storybook-and-its-addons-bdadaf73b4c3'
+            target='_blank'
           >
             <h3>Storybook and its Addons</h3>
-            <p className="mb-0 text-xs">Sep 27, 2020</p>
+            <p className='mb-0 text-xs'>Sep 27, 2020</p>
+          </a>
+          <a
+            className='my-5  anchor d-block'
+            href='https://medium.com/@gokul.srinivasan22/react-hooks-and-its-advantages-c54471da806'
+            target='_blank'
+          >
+            <h3>React Hooks and its Advantages</h3>
+            <p className='mb-0 text-xs'>Jun 20, 2020</p>
+          </a>
+          <a
+            className='my-5  anchor d-block'
+            href='https://medium.com/@gokul.srinivasan22/lets-explore-about-react-js-fb5ce4ebb78c'
+            target='_blank'
+          >
+            <h3>What is React JS</h3>
+            <p className='mb-0 text-xs'>Apr 20, 2020</p>
           </a>
         </div>
       </div>
